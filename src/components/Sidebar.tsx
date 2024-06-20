df2d7173738105fda6cc0f3a75fc1ff132b07410
Ankur1493 <ankursharma1493@gmail.com>
2024-06-20 17:40:32 +0530

    modified the theme

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -30,7 +30,7 @@ export const Sidebar = () => {
             animate={{ x: 0 }}
             transition={{ duration: 0.2, ease: "linear" }}
             exit={{ x: -200 }}
-            className="px-6  z-[100] py-10 bg-neutral-100 max-w-[14rem] lg:w-fit  fixed lg:relative  h-screen left-0 flex flex-col justify-between"
+            className="px-6  z-[100] py-10 bg-gray-950 max-w-[14rem] lg:w-fit  fixed lg:relative  h-screen left-0 flex flex-col justify-between"
           >
             <div className="">
               <SidebarHeader />
@@ -68,7 +68,7 @@ export const Navigation = ({
           onClick={() => isMobile() && setOpen(false)}
           className={twMerge(
             "text-secondary hover:text-primary transition duration-200 flex items-center space-x-2 py-2 px-2 rounded-md text-sm",
-            isActive(link.href) && "bg-white shadow-lg text-primary"
+            isActive(link.href) && "bg-gray-800 shadow-lg text-primary"
           )}
         >
           <link.icon
