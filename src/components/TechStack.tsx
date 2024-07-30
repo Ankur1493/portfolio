8726f3db593910e9a75e57e85168f6d60751f5fd
Ankur1493 <ankursharma1493@gmail.com>
2024-07-31 05:10:32 +0530

    build errors resolved

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -11,9 +11,9 @@ export const TechStack = () => {
       >
         Tech Stack
       </Heading>
-      <div className="grid grid-cols-4 gap-3">
+      <div className="grid grid-cols-3 gap-3 sm:grid-cols-4 sm:gap-3">
         {stack.map((item, index) => (
-          <p key={index} className="bg-gray-950 hover:text-white hover:opacity-75 rounded-full py-3 px-4 text-primary text-md text-center">
+          <p key={index} className="bg-gray-950 hover:text-white hover:opacity-75 rounded-full py-3 px-4 text-primary text-sm sm:text-md text-center">
             {item}
           </p>
         ))}
